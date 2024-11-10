f7054ee3d234b3f1bdf8820d19d5b855981bb556
Felipe Bonilha <bonilhfe@amazon.com>
2024-11-10 15:15:29 -0300

    add support to multiple quantities per item

--- src/js/snack-bar.js
+++ src/js/snack-bar.js
@@ -2,19 +2,35 @@ import axios from 'axios';
 
 async function getCurrentCart() {
     try {
-      const { data } = await axios.get("http://localhost:3000/items_in_cart");
-      console.log("items_in_cart", data);
+      const { data: itemsInCart } = await axios.get("http://localhost:3000/items_in_cart");
+      console.log("items_in_cart", itemsInCart);
     } catch (error) {
       console.log("error", error);
     }
 }
 
-async function addItemToCart(item) {
+async function addItemToCart(newItem) {
+    const { data: itemsInCart } = await axios.get("http://localhost:3000/items_in_cart");
+    console.log("items_in_cart", itemsInCart);
+
+    foundItem = itemsInCart.find(item => item.id === newItem.id);
+
     try {
-      await axios.post("http://localhost:3000/items_in_cart", item)
-    } catch (error) {
-      console.log("error", error);
-    }
+
+        if (foundItem) {
+          foundItem.quantity += newItem.quantity;
+          await axios.put(`http://localhost:3000/items_in_cart/${foundItem.id}`, foundItem);
+        } else {
+          await axios.post("http://localhost:3000/items_in_cart", newItem);
+        }
+      // try {
+      //   await axios.post("http://localhost:3000/items_in_cart", item)
+      // } catch (error) {
+      //   console.log("error", error);
+      // }
+  } catch (error) {
+    console.log("error", error);
+  }
 }
 
 // include functions on global scope of the window
